2adbd1d8038d2e99e4d99cb270986647759533f1
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-03-03 22:31:33 -0500

    I wrote a code that ass one point per "A" press, and resets per "B" press.

--- main.ts
+++ main.ts
@@ -5,4 +5,29 @@
  * This program adds one point everytime "A" is clicked, when "B" is clicked the points reser
 */
 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+const cookies:number = 0
+let cookieAmount:number = cookies
 
+/* This code adds a point to total amount of "cookies" on button "A" press and displays 
+* the number of points.
+*/
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    cookieAmount = cookieAmount + 1
+basic.showNumber(cookieAmount)
+})
+
+/* This code resets number of points on button "B" press and displays number of points, after 
+* three seconds it will ruturn to happy face.
+*/
+
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    cookieAmount = 0
+    basic.showNumber(cookieAmount)
+    basic.pause(3000)
+    basic.showIcon(IconNames.Happy)
+})
